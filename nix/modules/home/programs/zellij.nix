8a18d2ec8a9716ad1ce0ab002945046c4397b0c3
Jonas Fierlings <fnoegip@gmail.com>
2025-01-26 18:00:27 +0100

    feat(kamino): Install zellij configuration

---
+++ nix/modules/home/programs/zellij.nix
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2025 Jonas Fierlings <fnoegip@gmail.com>
+#
+# SPDX-License-Identifier: 0BSD
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkEnableOption
+    ;
+  cfg = config.croissant.programs.zellij;
+in
+{
+  _file = ./zellij.nix;
+
+  options.croissant.programs = {
+    zellij = {
+      enable = mkEnableOption "set up zellij";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs = {
+      zellij = {
+        enable = true;
+      };
+    };
+  };
+}
