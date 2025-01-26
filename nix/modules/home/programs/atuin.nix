ce001389e0d0d15ef0c3dbad80bab9b90aede47f
Jonas Fierlings <fnoegip@gmail.com>
2025-01-26 17:33:56 +0100

    feat(kamino): Install atuin configuration

---
+++ nix/modules/home/programs/atuin.nix
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2025 Jonas Fierlings <fnoegip@gmail.com>
+#
+# SPDX-License-Identifier: 0BSD
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkEnableOption
+    ;
+  cfg = config.croissant.programs.atuin;
+in
+{
+  _file = ./atuin.nix;
+
+  options.croissant.programs = {
+    atuin = {
+      enable = mkEnableOption "set up atuin";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs = {
+      atuin = {
+        enable = lib.mkDefault true;
+        flags = [ "--disable-up-arrow" ];
+        settings = {
+          history_filter = [
+            "^[bc]at"
+            "^cd"
+            "^exit"
+            "^export [0-9a-zA-Z\\-_]+?[-_]TOKEN="
+            "^ls"
+            "glpat-[0-9a-zA-Z\\-_]{20}"
+          ];
+        };
+      };
+    };
+  };
+}
