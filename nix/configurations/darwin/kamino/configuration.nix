3a5326d34b721381a82433ae6847a404f58ca2ea
Jonas Fierlings <fnoegip@gmail.com>
2025-02-19 21:32:42 +0100

    chore(kamino): Enable linux builder

--- nix/configurations/darwin/kamino/configuration.nix
+++ nix/configurations/darwin/kamino/configuration.nix
@@ -24,6 +24,35 @@
       ];
     };
 
+    nix = {
+      linux-builder = {
+        enable = true;
+        ephemeral = true;
+        maxJobs = 4;
+
+        config = {
+          virtualisation = {
+            darwin-builder = {
+              diskSize = 40 * 1024;
+              memorySize = 8 * 1024;
+            };
+            cores = 6;
+          };
+        };
+      };
+
+      settings = {
+        trusted-users = [ "@admin" ];
+      };
+    };
+
+    launchd.daemons.linux-builder = {
+      serviceConfig = {
+        StandardOutPath = "/var/log/darwin-builder.log";
+        StandardErrorPath = "/var/log/darwin-builder.log";
+      };
+    };
+
     networking = {
       computerName = "kamino";
     };
