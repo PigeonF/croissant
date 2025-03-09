a1e7616f55c22c6888cdfae0574d1cdcce8700d3
Jonas Fierlings <fnoegip@gmail.com>
2025-03-09 17:56:45 +0100

    chore: Fix generation of home-manager profile

--- nix/configurations/home/pigeonf/home.nix
+++ nix/configurations/home/pigeonf/home.nix
@@ -27,6 +27,7 @@
         if pkgs.stdenv.hostPlatform.isDarwin then "/Users/${userName}" else "/home/${userName}";
 
       sessionVariablesExtra = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin ''
+
         # Overwrite SSH_AUTH_SOCK unless we are using a SSH connection.
         if [ -z "''${SSH_CLIENT:-}" ] && [ -z "''${SSH_CONNECTION}" ] && [ -z "''${SSH_TTY}" ]; then
           export SSH_AUTH_SOCK="${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
