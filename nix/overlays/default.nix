57d10be5577d1bb0bf748d0b79181bc08b30edc7
Jonas Fierlings <fnoegip@gmail.com>
2025-04-11 11:51:57 +0200

    feat(phoebe): Enable macOS vms via tart

---
+++ nix/overlays/default.nix
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2025 Jonas Fierlings <fnoegip@gmail.com>
+#
+# SPDX-License-Identifier: 0BSD
+{ nixpkgs, ... }:
+
+let
+  inherit (nixpkgs) lib;
+
+  overlays = {
+    gitlab-runner = _: prev: {
+      # https://gitlab.com/gitlab-org/gitlab-runner/-/issues/38262
+      gitlab-runner = prev.gitlab-runner.overrideAttrs (
+        finalAttrs: previousAttrs: {
+          ldflags = previousAttrs.ldflags ++ [
+            "-B gobuildid"
+            "-buildid 9037CDCD-5315-31E5-FF83-EDD32673FD6E"
+          ];
+        }
+      );
+    };
+  };
+in
+
+overlays // { default = lib.composeManyExtensions (builtins.attrValues overlays); }
